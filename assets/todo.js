f091d83bc75b31c38d2bf27cbc5b1e0474a0c0c5
Elena Skakovskaya <eoskakovskaya@mail.ru>
2018-05-28 23:29:49 +0300

    add static, actualize README and index page

--- assets/todo.js
+++ assets/todo.js
@@ -1,42 +1,5 @@
-var vm = new Vue({
-    el: '#app',
-    data: {
-        todoList: [],
-        message: '',
-        storageIsAvailable: false
-    },
-    methods: {
-        addItem: function(text) {
-            this.todoList.push({text: text, done: false});
-            if (this.storageIsAvailable) {
-                var ls = getStorage();
-                ls.push({text: text, done: false});
-                localStorage.setItem('todoList', JSON.stringify(ls));
-            }
-            document.getElementById('todoInput').focus();
-        },
-        changeStatus: function(index) {
-            console.log(index);
-            if (this.storageIsAvailable){
-                var ls = getStorage();
-                console.log(ls[index].done);
-                ls[index].done = !this.todoList[index].done;
-                console.log(ls[index].done);
-                localStorage.setItem('todoList', JSON.stringify(ls));
-            }
-        }
-    },
-    mounted: function () {
-        this.storageIsAvailable = checkStorage();
-        if (this.storageIsAvailable) {
-            this.todoList = getStorage();
-        }
-        document.getElementById('todoInput').focus();
-
-    }
-});
-
-function storageAvailable(type) {
+function storageIsAvailable(type) {
+    console.log('storageIsAvailable?');
     try {
         var storage = window[type],
             x = '__storage_test__';
@@ -45,38 +8,27 @@ function storageAvailable(type) {
         return true;
     }
     catch (e) {
-        return e instanceof DOMException && (
-                // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            storage.length !== 0;
+        return false;
     }
 }
 
-function checkStorage() {
-    if (storageAvailable('localStorage')) {
+function cleanStorage(storageIsAvailable) {
+    if (storageIsAvailable) {
         var todoList = JSON.parse(localStorage.getItem('todoList'));
         for (var i = todoList.length-1; i >= 0; i--){
             if (todoList[i].done === true) {
-                todoList.splice(i, 1);
+                //todoList.splice(i, 1);
             }
         }
         localStorage.setItem('todoList', JSON.stringify(todoList));
-        return true;
 
         /*localStorage.setItem('todoList', JSON.stringify([{ text: 'learn AngularJS', done: true }]));
-         var todoList = JSON.parse(localStorage.getItem('todoList'));
+        var todoList = JSON.parse(localStorage.getItem('todoList'));
          todoList.push({ text: 'learn AngularJS', done: true });
          todoList.push({ text: 'build an AngularJS app', done: false });
          localStorage.setItem('todoList', JSON.stringify(todoList));
          console.log(localStorage);*/
+        return true;
     }
     else {
         alert('Недоступно локальное хранилище. После перезагрузки страницы все данные будут потеряны.');
@@ -87,3 +39,52 @@ function checkStorage() {
 function getStorage() {
     return JSON.parse(localStorage.getItem('todoList'));
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var store = storageIsAvailable();
+        cleanStorage();
+    console.log(store);
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            todoList: [],
+            message: '',
+            storageIsAvailable: false
+        },
+        methods: {
+            addItem: function(text) {
+                if (text) {
+                    this.todoList.push({text: text, done: false});
+                    if (this.storageIsAvailable) {
+                        var ls = getStorage();
+                        ls.push({text: text, done: false});
+                        localStorage.setItem('todoList', JSON.stringify(ls));
+                    }
+                }
+            },
+            changeStatus: function(index) {
+                console.log(index);
+                if (this.storageIsAvailable){
+                    var ls = getStorage();
+                    console.log(ls[index].done);
+                    ls[index].done = !this.todoList[index].done;
+                    console.log(ls[index].done);
+                    localStorage.setItem('todoList', JSON.stringify(ls));
+                }
+            }
+        },
+        mounted: function () {
+            this.storageIsAvailable = storageIsAvailable();
+            this.todoList = getStorage();
+            console.log("mounted");
+            //document.getElementById('todoInput').focus();
+
+        },
+        updated: function () {
+            console.log("updated");
+        },
+        beforeCreate: function () {
+            console.log("beforeCreate");
+        }
+    });
+});
